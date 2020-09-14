4539bd0c6969e69ff76896f3666c64b42d887ae4
daniellelshen <daniellelshen@gmail.com>
2020-09-14 12:40:32 -0700

    Create ud_counter.v

---
+++ Dani code/Lab1/srcs/ud_counter.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/07/2020 01:17:38 PM
+// Design Name: 
+// Module Name: ud_counter
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ud_counter(
+
+    );
+endmodule
